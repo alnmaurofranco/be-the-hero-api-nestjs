ff369cc5db6e0c5ce3f5512c6f0cfee16353c015
alnmaurofranco <jefeson23@live.com>
2021-12-23 05:58:11 -0300

    :tada: begin a new project

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
+import { NestApplicationOptions } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('/api');
+  const options: NestApplicationOptions = {
+    cors: {
+      origin: '*',
+    },
+  };
+
+  const app = await NestFactory.create(AppModule, options);
+  app.setGlobalPrefix('api');
   await app.listen(3000);
 }
 bootstrap();
