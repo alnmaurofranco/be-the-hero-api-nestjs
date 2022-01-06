ffca705df446ceb83a2a7c09d656e52abf04a791
alnmaurofranco <jefeson23@live.com>
2022-01-06 20:02:49 -0300

    add: configuration on/off SSL

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,22 @@ import { NestApplicationOptions, ValidationPipe } from '@nestjs/common';
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaClientExceptionFilter } from './infra/prisma/prisma-client-exception.filter';
+import { readFileSync } from 'fs';
 
 async function bootstrap() {
+  const certificate = readFileSync(__dirname + '/../localhost.crt');
+  const keyfile = readFileSync(__dirname + '/../localhost.key');
+
+  const httpsOptions = {
+    cert: certificate,
+    key: keyfile,
+  };
+
   const options: NestApplicationOptions = {
     cors: {
       origin: '*',
     },
+    httpsOptions: process.env.SSL_ENABLE === 'true' ? httpsOptions : undefined,
   };
 
   const app = await NestFactory.create(AppModule, options);
